c2fc7215254e8ae9933fc7914745aa0f60565285
Leenasri2000 <84555889+Leenasri2000@users.noreply.github.com>
2021-05-21 10:31:49 +0530

    Update overlap.m

--- overlap.m
+++ overlap.m
@@ -1,8 +1,8 @@
 clc;
 clear all;
 close all;
-bboxA = [594 166 381 102]; 
-bboxB = [825, 164, 123, 77];
+bboxA = [594 166 381 102]; %bounding box value of extracted ROI
+bboxB = [825, 164, 123, 77]; %bounding box value of ground truth ROI
 I1 = imread('D_4034Mask.jpg');
 I2 = imresize(I1,[1000,1000]);
 RGB = insertShape(I2,'FilledRectangle',bboxA,'Color','red');
