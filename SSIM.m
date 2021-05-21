85ca6cb585e1406b6fa442514a092d57a8857892
Leenasri2000 <84555889+Leenasri2000@users.noreply.github.com>
2021-05-21 10:28:22 +0530

    Update SSIM.m

--- SSIM.m
+++ SSIM.m
@@ -1,8 +1,8 @@
 clc;
 clear all;
 close all;
-bboxA = [306, 384, 338, 152]; 
-bboxB = [247, 363, 409, 206];
+bboxA = [306, 384, 338, 152]; %bounding box value of extracted ROI
+bboxB = [247, 363, 409, 206]; %bounding box value of ground truth ROI
 I = imread("D_4181_1.LEFT_CC_Mask.jpg");
 I2 = imresize(I,[1000,1000]);
 RGB = insertShape(I2,'FilledRectangle',bboxA,'Color','red');
