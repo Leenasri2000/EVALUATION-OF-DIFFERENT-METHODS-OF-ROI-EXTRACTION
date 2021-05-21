0306bedc6e825758693d640706c550db1968bff4
Leenasri2000 <84555889+Leenasri2000@users.noreply.github.com>
2021-05-21 10:29:05 +0530

    Update Superimpose.m

--- Superimpose.m
+++ Superimpose.m
@@ -1,4 +1,4 @@
-A = imread('B_3412_BO.png');
-B = imrotate('B_3412_1.LEFT_MLO_MASK.jpg');
+A = imread('EXTRACTED_IMAGE.png');
+B = imrotate('GROUNDTRUTH.jpg');
 C = imfuse(A,B,'blend','Scaling','joint');
 imshow(C)
