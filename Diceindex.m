b2385bcff78d2644f5850f12e1ef65bae4a91a84
Leenasri2000 <84555889+Leenasri2000@users.noreply.github.com>
2021-05-21 10:19:21 +0530

    Update Diceindex.m

--- Diceindex.m
+++ Diceindex.m
@@ -1,11 +1,11 @@
 clc;
 clear all;
 close all;
-A = imread('B_3022Wbb.png');
+A = imread('EXTRACTED_IMAGE.png');
 I = imresize(A,[1000,1000]);
 figure(1);
 imshow(I)
 title('Original Image')
-BW_groundTruth = imread('B_3022maskbb.png');
+BW_groundTruth = imread('GROUNDTRUTH.png');
 I2 = imresize(BW_groundTruth,[1000,1000]);
 dice = 2*nnz(I&I2)/(nnz(I) + nnz(I2))
